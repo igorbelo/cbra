f6b81fa41f53c98afad432c46342b53a8467de1a
Igor Belo <igor@wine.com.br>
2015-02-05 16:05:25 -0200

    mounting mail_subscription route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount MailSubscription::Engine, at: '/mail-subscription'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
