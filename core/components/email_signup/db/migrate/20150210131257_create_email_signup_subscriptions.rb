62de1301f48fd708f50540f66edea17d0c49f5a6
Igor Belo <igor@wine.com.br>
2015-02-10 18:07:35 -0200

    deleting components from core

--- core/components/email_signup/db/migrate/20150210131257_create_email_signup_subscriptions.rb
+++
@@ -1,9 +0,0 @@
-class CreateEmailSignupSubscriptions < ActiveRecord::Migration
-  def change
-    create_table :email_signup_subscriptions do |t|
-      t.string :email
-
-      t.timestamps
-    end
-  end
-end
--- core/components/email_signup/test/models/email_signup/subscription_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module EmailSignup
-  class SubscriptionTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
