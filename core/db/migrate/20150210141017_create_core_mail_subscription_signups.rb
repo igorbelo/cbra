baf3c4f70e246c51fe978cf2f33bf9f03d0e7d2a
Igor Belo <igor@wine.com.br>
2015-02-11 10:45:09 -0200

    deleting old migration

--- core/db/migrate/20150210141017_create_core_mail_subscription_signups.rb
+++
@@ -1,9 +0,0 @@
-class CreateCoreMailSubscriptionSignups < ActiveRecord::Migration
-  def change
-    create_table :core_mail_subscription_signups do |t|
-      t.string :email
-
-      t.timestamps
-    end
-  end
-end
