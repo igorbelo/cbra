272162550f8a6764f355d87e05dd3fddc1791185
Igor Belo <igor@wine.com.br>
2015-02-11 11:38:58 -0200

    bugfix migration (number -> integer)

--- core/db/migrate/20150211132327_create_core_adresses.rb
+++ core/db/migrate/20150211132327_create_core_adresses.rb
@@ -2,7 +2,7 @@ class CreateCoreAdresses < ActiveRecord::Migration
   def change
     create_table :core_adresses do |t|
       t.string :zip
-      t.number :number
+      t.integer :number
       t.references :customer, index: true
 
       t.timestamps
